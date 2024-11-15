194fa81b3e980f150722ec100eaba4adbcdf3ee3
Dennis <dennisadev@gmail.com>
2024-11-15 13:05:01 -0500

    inital commit

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,31 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Random x = new Random();
+            int favNumber = x.Next(1, 11);
+            int userInput = 0;
+
+
+            while (userInput != favNumber)
+            {
+
+                Console.WriteLine("Guess my favorite number?");
+                userInput = int.Parse(Console.ReadLine());
+
+                if (userInput < favNumber)
+                {
+                    Console.WriteLine("Too Low");
+                }
+                else if (userInput > favNumber)
+                {
+                    Console.WriteLine("Too High");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!!!");
+                }
+            }
+
         }
     }
 }
